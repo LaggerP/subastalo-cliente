8b988da8aa47a157dbb957b72b8c81448c0a78cd
LaggerP <pablo_lagger@hotmail.com>
2021-05-16 13:50:30 -0300

    Created a new Expo app

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Me from './components/me'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>
-        <Me/>
-      </Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -15,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#cccc',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
