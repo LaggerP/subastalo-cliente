c5fb4a123be38017c1a27c4b82187398c9fb88a6
LaggerP <pablo_lagger@hotmail.com>
2021-06-09 18:56:46 -0300

    solved some visuals errors

--- src/Components/MetodoDePago/ModalMetodosPago.js
+++ src/Components/MetodoDePago/ModalMetodosPago.js
@@ -20,8 +20,8 @@ export const ModalMetodosPago = ({modalData, setShowModal, navigation}) => {
 
   const toggleOverlay = () => {
     setShowModal({...modalData, visible: !visible});
-    if(modalData.icon=='newMP'){
-        navigation.goBack();
+    if (modalData.icon === 'newMP') {
+      navigation.goBack();
     }
   };
 
@@ -36,10 +36,10 @@ export const ModalMetodosPago = ({modalData, setShowModal, navigation}) => {
                  width: 350,
                  borderRadius: 10
                }}>
-        <Text h4 style={{marginTop: 20}}>{title}</Text>
+        <Text h4 style={{marginTop: 20, textAlign:'center'}}>{title}</Text>
         <Text style={{marginHorizontal: 18, marginTop: 20, textAlign: 'center'}}>{msg}</Text>
         <Image
-          style={{width: 130, height: 130, marginVertical:30}}
+          style={{width: 130, height: 130, marginVertical: 30}}
           source={iconToShow[icon]}
         />
         <Button
