e431b6dab3c515f0de3992f789caf6be3f884aab
Tamara Cabello <tamaracabello7@gmail.com>
2021-06-10 16:37:27 -0300

    persistencia arreglada + limitacion en catalogo al no iniciar sesion

--- src/Screens/Login/LoginScreen.js
+++ src/Screens/Login/LoginScreen.js
@@ -3,9 +3,6 @@ import React from 'react';
 // Navigator
 import { createStackNavigator } from "@react-navigation/stack";
 
-// Provider
-import { DataProvider } from "../../context/DataContext";
-
 // Components
 import Login from "./Login";
 import RestablecerPrimer from "./RestablecerPrimer";
@@ -18,16 +15,14 @@ const LoginStack = createStackNavigator();
 
 const LoginScreen = () => {
     return (
-        <DataProvider>
-            <LoginStack.Navigator>
-                <LoginStack.Screen name='Login' component={Login} options={{title: '', headerLeft: null, headerTransparent: true}}/>
-                <LoginStack.Screen name='RestablecerPrimer' component={RestablecerPrimer} options={{title: '', headerLeft: null, headerTransparent: true}}/>
-                <LoginStack.Screen name='RestablecerSegundo' component={RestablecerSegundo} options={{title: '', headerLeft: null, headerTransparent: true}}/>
-                <LoginStack.Screen name='RegistroUno' component={RegistroUno} options={{title: '', headerLeft: null, headerTransparent: true}}/>
-                <LoginStack.Screen name='RegistroDos' component={RegistroDos} options={{title: '', headerLeft: null, headerTransparent: true}}/>
-                <LoginStack.Screen name='RegistroExito' component={RegistroExito} options={{title: '', headerLeft: null, headerTransparent: true}}/>
-            </LoginStack.Navigator>
-        </DataProvider>
+        <LoginStack.Navigator>
+            <LoginStack.Screen name='Login' component={Login} options={{ title: '', headerLeft: null, headerTransparent: true }} />
+            <LoginStack.Screen name='RestablecerPrimer' component={RestablecerPrimer} options={{ title: '', headerLeft: null, headerTransparent: true }} />
+            <LoginStack.Screen name='RestablecerSegundo' component={RestablecerSegundo} options={{ title: '', headerLeft: null, headerTransparent: true }} />
+            <LoginStack.Screen name='RegistroUno' component={RegistroUno} options={{ title: '', headerLeft: null, headerTransparent: true }} />
+            <LoginStack.Screen name='RegistroDos' component={RegistroDos} options={{ title: '', headerLeft: null, headerTransparent: true }} />
+            <LoginStack.Screen name='RegistroExito' component={RegistroExito} options={{ title: '', headerLeft: null, headerTransparent: true }} />
+        </LoginStack.Navigator>
     )
 };
 
